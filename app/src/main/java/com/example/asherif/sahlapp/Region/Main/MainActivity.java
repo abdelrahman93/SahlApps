646eeef6a2e020389145064fc927c8a58fe4c0be
Abdel-rahman Sherif <a.shreef93@gmail.com>
2018-10-24 11:18:47 +0200

    update profile

--- app/src/main/java/com/example/asherif/sahlapp/Region/Main/MainActivity.java
+++ app/src/main/java/com/example/asherif/sahlapp/Region/Main/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.asherif.sahlapp.Region.Main;
 
 import android.graphics.Color;
-import android.os.Build;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Toolbar;
 
 import com.example.asherif.sahlapp.R;
+import com.example.asherif.sahlapp.Region.Main.profile.fragment_profile;
 
-import java.util.ArrayList;
-import java.util.List;
+//updated version
 
 public class MainActivity extends AppCompatActivity implements MainView {
     private ViewPager viewPager;
